1c3100d5494ab2a4b2bfb4b8728b4c4f8b466bc8
markvassell <mdvy96@mail.missorui.edu>
2018-04-24 03:08:44 -0500

    Wrote the quick sort algorithm. I'll document it when I get home from work.
    Signed-off-by: markvassell <mdvy96@mail.missorui.edu>

---
+++ Algorithms/quick_sort.py
@@ -0,0 +1,50 @@
+from random import sample
+
+
+def quick_sort(array, start=0, end=None):
+    new_array = list(array)
+    if len(array) == 1:
+        return array
+    elif start < end:
+        part = partition(new_array, start, end)
+        quick_sort(new_array, start, part - 1)
+        quick_sort(new_array, part + 1, end)
+
+    return new_array
+
+
+def partition(array, start, end):
+    #  using the middle index as the pivot
+    mid = ((end + start) // 2)
+    #  print('Pivot: ', array[mid])
+    #  moves the pivot to the end of the list
+    swap(array, mid, end)
+    i = start - 1
+    j = start
+
+    pivot = array[end]
+
+    #  Learned this from two sources:
+    #  KC Ang https://www.youtube.com/watch?v=MZaf_9IZCrc
+    #  RobEdwardsSDSU https://www.youtube.com/watch?v=ZHVk2blR45Q
+
+    while j < end:
+        if array[j] < pivot:
+            i += 1
+            swap(array, i, j)
+        j += 1
+    swap(array, i+1, end)
+    return i + 1
+
+
+def swap(array, pos1, pos2):
+    temp = array[pos1]
+    array[pos1] = array[pos2]
+    array[pos2] = temp
+
+
+if __name__ == '__main__':
+    data = sample(range(1, 1000), 5)
+    print('Unsorted List: ', data)
+    sorted_list = quick_sort(data, 0, len(data)-1)
+    print("Sorted List: ", sorted_list)
