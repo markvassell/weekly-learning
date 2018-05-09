678ef475cc79919e1cfd6061e5afcb4e20e71f6c
markvassell <mdvy96@mail.missorui.edu>
2018-05-08 20:57:12 -0500

    Covered Buy and sell stock with k transactions Dynamic Programming
    Signed-off-by: markvassell <mdvy96@mail.missorui.edu>

--- Projects/snake.py
+++ Projects/snake.py
@@ -22,11 +22,17 @@ class Food:
 
 class Player:
     def __init__(self):
-        self.speed = 5
+        self.x, self.y = randint(20, 580), randint(20, 580)
+        self.speed = 1
         self.blocks = 1
         self.height = BLOCK_SIZE
         self.width = BLOCK_SIZE
-        self.snake = pygame.Rect(randint(5,595), randint(5,595), self.height, self.width)
+        self.snake_positions = [pygame.Rect(self.x, self.y, self.height, self.width),
+                                pygame.Rect(self.x - 10, self.y, self.height, self.width),
+                                pygame.Rect(self.x - 20, self.y, self.height, self.width),
+                                pygame.Rect(self.x - 30, self.y, self.height, self.width)]
+        self.snake_head = self.snake_positions[0]
+        #self.snake = pygame.Rect(self.x, self.y, self.height, self.width)
         self.score = 0
         self.direction = R
 
@@ -35,14 +41,14 @@ class Player:
         return self.blocks
 
     def check_position(self):
-        if self.snake.bottom > 600:
-            self.snake.top = 0
-        elif self.snake.top < 0:
-            self.snake.bottom = 600
-        elif self.snake.left < 0:
-            self.snake.right = 600
-        elif self.snake.right > 600:
-            self.snake.left = 0
+        if self.snake_head.bottom > 600:
+            self.snake_head.top = 0
+        elif self.snake_head.top < 0:
+            self.snake_head.bottom = 600
+        elif self.snake_head.left < 0:
+            self.snake_head.right = 600
+        elif self.snake_head.right > 600:
+            self.snake_head.left = 0
 
     def set_direction(self, new_direction):
         direction = (0,0)
@@ -58,8 +64,11 @@ class Player:
         elif new_direction == D:
             direction = (0, self.speed)
 
+        center = 0,0
+        for index, rect in enumerate(self.snake_positions):
+            rect.move_ip(direction)
 
-        self.snake.move_ip(direction)
+        #self.snake.move_ip(direction)
 
     def increment_score(self):
         self.score += 1
@@ -68,7 +77,7 @@ class Player:
         return self.score
 
     def increase_speed(self):
-        self.speed += 1
+        self.speed += .25
 
 class Game:
     def __init__(self):
@@ -91,11 +100,13 @@ class Game:
     def update_window(self):
         self.game_window.fill(self.colors.black)
         pygame.draw.rect(self.game_window, self.colors.red, self.food.food)
-        pygame.draw.rect(self.game_window, self.colors.green, self.snake.snake, 2)
+        # pygame.draw.rect(self.game_window, self.colors.green, self.snake.snake, 2)
+        for rect in self.snake.snake_positions:
+            pygame.draw.rect(self.game_window, self.colors.green, rect, 2)
         pygame.display.update()
 
     def check_for_eat_event(self):
-        if self.snake.snake.colliderect(self.food.food):
+        if self.snake.snake_head.colliderect(self.food.food):
             self.food = Food()
             self.snake.increment_score()
             print("Current Score: %d" % self.snake.score)
@@ -118,7 +129,7 @@ class Game:
                         self.current_move = L
                     if event.key == pygame.K_RIGHT and self.current_move != L:
                         self.current_move = R
-
+            print(self.snake.speed)
             self.check_for_eat_event()
             self.snake.check_position()
             self.snake.set_direction(self.current_move)
